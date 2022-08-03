99edacfde1ae29eaab8d2f334236c725ca046ddc
ritik sinha <ritiksinha.zero4@gmail.com>
2022-08-03 22:10:33 +0530

    dom manipulation

---
+++ dom.js
@@ -0,0 +1,16 @@
+// //console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.tittle);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+
+// // document.all[10].textContent
+
+// console.log(document.forms);
+// console.log(document.links);
+
+// console.log(document.images);
+
