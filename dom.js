4fcbb2c29178158235afb7c24213b70aef2e768a
ritik sinha <ritiksinha.zero4@gmail.com>
2022-08-05 21:09:57 +0530

    QUERYSELECTORALL

--- dom.js
+++ dom.js
@@ -62,20 +62,42 @@
 // }
 
 // QUERYSELECTOR //
-var header = document.querySelector('#main-header');
-header.style.borderBotton = 'solid 4px grey';
+// var header = document.querySelector('#main-header');
+// header.style.borderBotton = 'solid 4px grey';
 
-var input = document.querySelector('input');
-input.value = "Hello World!";
+// var input = document.querySelector('input');
+// input.value = "Hello World!";
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value = "SEND";
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red'; 
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red'; 
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility = 'hidden';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden';
+
+// QUERYSELECTORALL //
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = "Hello";
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0; i<odd.length; i++){
+   odd[i].style.backgroundColor = "green";
+}
+
+let li = document.querySelectorAll('.list-group-item');
+console.log(li);
+li[1].style.color = 'green';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+// for(let i=0; i<odd.length; i++){
+//   odd[i].style.backgroundColor = "green";
+//   even[i].style.backgroundColor = "#ccc";
+// }
