4b2843f0751ee669ddafc19c273e0bf20cc98dcd
ritik sinha <ritiksinha.zero4@gmail.com>
2022-08-05 15:41:58 +0530

    get element by class name

--- dom.js
+++ dom.js
@@ -17,19 +17,29 @@
 // console.log(document.images);
 
 // GETELEMENTBYID //
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-var main = document.querySelector('.title');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// var main = document.querySelector('.title');
 // console.log(headerTitle);
 // headerTitle.textContent = ' Hello';
 // headerTitle.innerText = 'Goodbye'
-main.style.fontWeight = 'bold';
-main.style.color = 'green';
-main.innerHTML = 'ADD ITEMS';
-header.style.borderBottom = 'solid 2px black';
+// main.style.fontWeight = 'bold';
+// main.style.color = 'green';
+// main.innerHTML = 'ADD ITEMS';
+// header.style.borderBottom = 'solid 2px black';
 
 // GETELEMENTSBYCLASSNAME //
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
-// items[1].textContent = 'Hello';
-// items[1].style.fontWeight = 'bold';
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold'; 
+items[1].style.backgroundColor = 'yellow';
+
+// error
+// items.style.backgroundColor = "light grey";
+
+for(var i = 0; i<items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4' ;
+}
+
