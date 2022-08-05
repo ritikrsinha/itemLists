9bc4c4bb2e0acdbda7290485224dfb5de88343fe
ritik sinha <ritiksinha.zero4@gmail.com>
2022-08-05 20:35:01 +0530

    GETELEMENTSBYTAGNAME

--- dom.js
+++ dom.js
@@ -29,24 +29,35 @@
 // header.style.borderBottom = 'solid 2px black';
 
 // GETELEMENTSBYCLASSNAME //
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold'; 
-items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold'; 
+// items[1].style.backgroundColor = 'yellow';
 
 // error
 // items.style.backgroundColor = "light grey";
 
-for(let i = 0; i<items.length; i++){
+// for(let i = 0; i<items.length; i++){
 
-items[i].style.backgroundColor = '#f4f4f4';
-}
-items[2].style.backgroundColor = 'green';
+// items[i].style.backgroundColor = '#f4f4f4';
+// }
+// items[2].style.backgroundColor = 'green';
 
-for(let i = 0; i<items.length; i++){
+// for(let i = 0; i<items.length; i++){
 
-    items[i].style.fontWeight = 'bold';
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold'; 
+li[1].style.backgroundColor = 'yellow';
+for(let i = 0; i<li.length; i++){
+li[i].style.backgroundColor = 'gray';
 }
 
